6f7ded96f329f8b2e0140ed4bae5fa84bf7361de
Pon Thanat <aaponaa2632@gmail.com>
2025-07-21 13:44:28 +0700

    fix: fix ex-3 code

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,9 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
-let minScore = scores[0];
-// Start coding here
+let minScore;
+//Start coding here
 for (let i=1;i<scores.length;i++) {
-    if (scores[i] < minScore) {
+    if (minScore === undefined || scores[i] < minScore) {
         minScore = scores[i]
     }
 }
